4612ab6f127541e9578d03dbab86a89b9cddf483
fcagalj <fcagalj@gmail.com>
2022-10-30 15:07:11 +0100

    default localization de

--- src/helpers/IfLocale.jsx
+++ src/helpers/IfLocale.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const IfLocale = ({ locale, children }) => (
-  <>
-    {`{{#if (equals locale "${locale}")}}`}
-    {children}
-    {`{{/if}}`}
-  </>
-)
+const IfLocale = ({ locale, children }) => {
+  // if locale is not defined return "de" version
+  if (locale === 'de') {
+    return (
+      <>
+        {`{{#if (equals locale "${locale}")}}`}
+        {children}
+        {`{{/if}}`}
+        {`{{#unless locale}}`}
+        {children}
+        {`{{/unless}}`}
+      </>
+    )
+  }
+  return (
+    <>
+      {`{{#if (equals locale "${locale}")}}`}
+      {children}
+      {`{{/if}}`}
+    </>
+  )
+}
 
 IfLocale.propTypes = {
   children: PropTypes.node,
