6a1a1035a8ecbe06f23069f31f806864d11ca301
MarinoDrazic <marinodrazic12@gmail.com>
2022-11-18 14:39:09 +0100

    Certificate_Approval_Report_Template to new layout

--- src/templates/Certificate_Approval_Report_Template/Body.jsx
+++ src/templates/Certificate_Approval_Report_Template/Body.jsx
@@ -1,61 +1,66 @@
 import React from 'react'
+import Card from '../../components/Card'
+import UnlessVariable from '../../helpers/UnlessVariable'
 import Variable from '../../helpers/Variable'
-import ButtonConfirm from '../../shared/ButtonConfirm'
-import IfVariable from '../../helpers/IfVariable'
-import Paragraph from '../../shared/Paragraph'
+import Paragraph from '../../components/Paragraph'
+import RequesterInfo from '../../components/RequesterInfo'
+import Spacer from '../../components/Spacer'
 import Each from '../../helpers/Each'
-import AboutTenera from '../../shared/AboutTenera'
-import BottomText from '../../shared/BottomText'
-import MarketingLayout from '../../shared/MarketingLayout'
-import MarketingHeader from '../../shared/MarketingHeader'
-import MarketingFooter from '../../shared/MarketingFooter'
+import IfLocale from '../../helpers/IfLocale'
+import IfVariable from '../../helpers/IfVariable'
+import AboutTenera from '../../components/AboutTenera'
+import LayoutHtml from '../../layouts/LayoutHtml'
+import FooterClassic from '../../layouts/FooterClassic'
 
-export const Layout = MarketingLayout
-export const Header = MarketingHeader
-export const Footer = MarketingFooter
+export const Layout = LayoutHtml
+export const Footer = FooterClassic
 
 const Body = () => (
   <>
-    <div className="section intro" style={{ padding: '10px' }}>
-      <table width="100%">
-        <tbody>
-          <div>
-            <p>
-              Guten Tag,
-              <br />
-              <Variable name="gcOrganizationName" />
-              hat einige Ihrer Bescheinigungen freigegeben.
-            </p>
-            <IfVariable name="approvedCertificateProposals">
-              <ButtonConfirm
-                title="Bescheinigungen einsehen"
-                hrefValue="{{approvedCertificateProposals.[0].certificateProposalUrl}}"
-              />
-            </IfVariable>
-            <br />
-            <br />
-            <Paragraph title="Kürzlich freigegebene Bescheinigungen">
-              <ul>
-                <Each iterator="approvedCertificateProposals">
-                  <li>
-                    <a
-                      href="{{this.certificateProposalUrl}}"
-                      style={{
-                        color: 'black',
-                      }}
-                    >
-                      <Variable name="this.filename" />
-                    </a>
-                  </li>
-                </Each>
-              </ul>
-            </Paragraph>
-          </div>
-        </tbody>
-      </table>
-    </div>
+    <Card
+      title="{{{gcOrganizationName}}} hat Bescheinigungen freigegeben"
+      ctaButtonTitle="Bescheinigungen einsehen"
+      ctaButtonUrl="{{approvedCertificateProposals.[0].certificateProposalUrl}}"
+    >
+      <UnlessVariable name="messageTemplate">
+        <Paragraph>
+          Guten Tag,
+          <br />
+          <Variable name="gcOrganizationName" />
+          hat einige Ihrer Bescheinigungen freigegeben.
+        </Paragraph>
+        <Paragraph title="Kürzlich freigegebene Bescheinigungen">
+          <ul>
+            <Each iterator="approvedCertificateProposals">
+              <li>
+                <a
+                  href="{{this.certificateProposalUrl}}"
+                  style={{
+                    color: 'black',
+                  }}
+                >
+                  <Variable name="this.filename" />
+                </a>
+              </li>
+            </Each>
+          </ul>
+        </Paragraph>
+      </UnlessVariable>
+      <Variable name="messageTemplate" />
+    </Card>
+    <Spacer size={32} />
+    <Card title={<IfLocale locale="de">Angefragte Bescheinigungen</IfLocale>}>
+      <IfVariable name="certificateTypeName">
+        <Paragraph>
+          <strong>{`{{certificateTypeName}}`}</strong>
+        </Paragraph>
+      </IfVariable>
+      <Variable name="messageTemplate" />
+    </Card>
+    <Spacer size={32} />
+    <RequesterInfo />
+    <Spacer size={32} />
     <AboutTenera />
-    <BottomText />
   </>
 )
 
