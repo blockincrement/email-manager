6f4f278f523d98ad334dfb1ebe644a4259f55c13
MarinoDrazic <marinodrazic12@gmail.com>
2022-11-21 13:42:12 +0100

    Certificate_Expiration_Report_Template to new layout

--- src/templates/Certificate_Expiration_Report_Template/Body.jsx
+++ src/templates/Certificate_Expiration_Report_Template/Body.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
+import Card from '../../components/Card'
+import UnlessVariable from '../../helpers/UnlessVariable'
 import Variable from '../../helpers/Variable'
-import ButtonConfirm from '../../shared/ButtonConfirm'
-import IfVariable from '../../helpers/IfVariable'
-import Paragraph from '../../shared/Paragraph'
+import Paragraph from '../../components/Paragraph'
+import RequesterInfo from '../../components/RequesterInfo'
+import Spacer from '../../components/Spacer'
 import Each from '../../helpers/Each'
-import BottomText from '../../shared/BottomText'
-import MarketingLayout from '../../shared/MarketingLayout'
-import MarketingHeader from '../../shared/MarketingHeader'
-import MarketingFooter from '../../shared/MarketingFooter'
+import IfLocale from '../../helpers/IfLocale'
+import IfVariable from '../../helpers/IfVariable'
+import AboutTenera from '../../components/AboutTenera'
+import LayoutHtml from '../../layouts/LayoutHtml'
+import FooterClassic from '../../layouts/FooterClassic'
 
-export const Layout = MarketingLayout
-export const Header = MarketingHeader
-export const Footer = MarketingFooter
+export const Layout = LayoutHtml
+export const Footer = FooterClassic
 
 const Body = () => (
   <>
-    <div className="section intro" style={{ padding: '10px' }}>
-      <table width="100%">
-        <tbody>
+    <Card
+      title="Abgelaufene oder bald ablaufende Bescheinigungen von {{{scCompanyName}}}"
+      ctaButtonTitle="Bescheinigungen einsehen"
+      ctaButtonUrl="{{expiredCertificatesAndDurations.[0].platformUrl}}/contacts/{{expiredCertificatesAndDurations.[0].companyId}}/?contactCompanyActiveTab=certificates"
+    >
+      <UnlessVariable name="messageTemplate">
+        <Paragraph>
           <div>
             <p>
               Hallo <Variable name="gcOrganizationName" />,
@@ -30,74 +36,80 @@ const Body = () => (
               <Variable name="scCompanyName" /> einsehen, indem Sie auf “Bescheingungen einsehen” klicken. Unten sehen
               Sie bereits eine Liste der betroffenen Bescheinigungen.
             </p>
-            <ButtonConfirm
-              title="Bescheinigungen einsehen"
-              hrefValue="{{expiredCertificatesAndDurations.[0].platformUrl}}/contacts/{{expiredCertificatesAndDurations.[0].companyId}}/?contactCompanyActiveTab=certificates"
-            />
-            <br />
-            <br />
-            <br />
-            <Paragraph title="Abgelaufene Bescheinigungen">
-              <Each iterator="expiredCertificatesAndDurations">
-                <ul style={{ listStyleType: 'none' }}>
-                  <li>
-                    Lief <Variable name="this.duration" /> ab
-                    <ul>
-                      <IfVariable name="this.certificateTypeName">
-                        <li>
-                          <Variable name="this.certificateTypeName" />
-                          <ul>
-                            <li>
-                              <a
-                                href="{{ this.platformUrl }}/contacts/{{this.companyId}}/?certificateId={{this.certificateId}}&contactCompanyActiveTab=certificates"
-                                style={{
-                                  color: 'black',
-                                }}
-                              >
-                                <Variable name="this.name" />
-                              </a>
-                            </li>
-                          </ul>
-                        </li>
-                      </IfVariable>
-                    </ul>
-                  </li>
-                </ul>
-              </Each>
-            </Paragraph>
-            <Paragraph title="Abgelaufene Bescheinigungen">
-              <Each iterator="expiringCertificatesAndDurations">
-                <ul style={{ listStyleType: 'none' }}>
-                  <li>
-                    Läuft <Variable name="this.duration" /> ab
-                    <ul>
-                      <IfVariable name="this.certificateTypeName">
-                        <li>
-                          <Variable name="this.certificateTypeName" />
-                          <ul>
-                            <li>
-                              <a
-                                href="{{ this.platformUrl }}/contacts/{{this.companyId}}/?certificateId={{this.certificateId}}&contactCompanyActiveTab=certificates"
-                                style={{
-                                  color: 'black',
-                                }}
-                              >
-                                <Variable name="this.name" />
-                              </a>
-                            </li>
-                          </ul>
-                        </li>
-                      </IfVariable>
-                    </ul>
-                  </li>
-                </ul>
-              </Each>
-            </Paragraph>
           </div>
-        </tbody>
-      </table>
-    </div>
-    <BottomText />
+        </Paragraph>
+        <Paragraph title="Abgelaufene Bescheinigungen">
+          <Each iterator="expiredCertificatesAndDurations">
+            <ul style={{ listStyleType: 'none' }}>
+              <li>
+                Lief <Variable name="this.duration" /> ab
+                <ul>
+                  <IfVariable name="this.certificateTypeName">
+                    <li>
+                      <Variable name="this.certificateTypeName" />
+                      <ul>
+                        <li>
+                          <a
+                            href="{{ this.platformUrl }}/contacts/{{this.companyId}}/?certificateId={{this.certificateId}}&contactCompanyActiveTab=certificates"
+                            style={{
+                              color: 'black',
+                            }}
+                          >
+                            <Variable name="this.name" />
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                  </IfVariable>
+                </ul>
+              </li>
+            </ul>
+          </Each>
+        </Paragraph>
+        <Paragraph title="Abgelaufene Bescheinigungen">
+          <Each iterator="expiringCertificatesAndDurations">
+            <ul style={{ listStyleType: 'none' }}>
+              <li>
+                Läuft <Variable name="this.duration" /> ab
+                <ul>
+                  <IfVariable name="this.certificateTypeName">
+                    <li>
+                      <Variable name="this.certificateTypeName" />
+                      <ul>
+                        <li>
+                          <a
+                            href="{{ this.platformUrl }}/contacts/{{this.companyId}}/?certificateId={{this.certificateId}}&contactCompanyActiveTab=certificates"
+                            style={{
+                              color: 'black',
+                            }}
+                          >
+                            <Variable name="this.name" />
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                  </IfVariable>
+                </ul>
+              </li>
+            </ul>
+          </Each>
+        </Paragraph>
+      </UnlessVariable>
+      <Variable name="messageTemplate" />
+    </Card>
+    <Spacer size={32} />
+    <Card title={<IfLocale locale="de">Angefragte Bescheinigungen</IfLocale>}>
+      <IfVariable name="certificateTypeName">
+        <Paragraph>
+          <strong>{`{{certificateTypeName}}`}</strong>
+        </Paragraph>
+      </IfVariable>
+      <Variable name="messageTemplate" />
+    </Card>
+    <Spacer size={32} />
+    <RequesterInfo />
+    <Spacer size={32} />
+    <AboutTenera />
   </>
 )
 
